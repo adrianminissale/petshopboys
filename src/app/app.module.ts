17aa4e3a51f5e65aa327390995af348aeb02ef58
Adrian Minissale <adrian.minissale@kindredgroup.com>
2022-09-10 13:43:48 +0200

    create initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { InitialStateComponent } from './initial-state/initial-state.component';
+import { EmptyStateComponent } from './empty-state/empty-state.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InitialStateComponent,
+    EmptyStateComponent,
+    SearchComponent,
+    ResultsComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/initial-state/initial-state.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-state',
+  templateUrl: './initial-state.component.html',
+  styleUrls: ['./initial-state.component.scss']
+})
+export class InitialStateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
